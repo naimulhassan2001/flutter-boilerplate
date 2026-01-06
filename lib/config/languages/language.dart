4eaeb24940ef12e3a4ddf825ac8ba833285bc71a
MD Naim Mia <developernaimul00@gmail.com>
2026-01-06 18:27:23 +0600

    update

--- lib/config/languages/language.dart
+++ lib/config/languages/language.dart
@@ -2,7 +2,6 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 
 class LocalConstants extends Translations {
   @override
-  // TODO: implement keys
   Map<String, Map<String, String>> get keys => {
         'en_US': {
           'No Data found': 'No Data found',
